6c02c29dd836dbc34b30ad7b786de949bf2afd7c
rizvandavudov <rizv>
2025-02-28 18:32:34 +0400

    1.01

--- app/src/main/java/com/example/speechtotext/InfoActivity.kt
+++ app/src/main/java/com/example/speechtotext/InfoActivity.kt
@@ -2,29 +2,33 @@ package com.example.speechtotext
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
+import com.example.speechtotext.databinding.ActivityInfoBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class InfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_info)
 
-        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
-        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        // View Binding aktiv edilir
+        binding = ActivityInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // ViewPager və TabLayout ilə əlaqələndirmə
         val sectionsPagerAdapter = SectionsPagerAdapter(this)
-        viewPager.adapter = sectionsPagerAdapter
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+        binding.viewPager.adapter = sectionsPagerAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             when (position) {
                 0 -> {
                     tab.text = "Haqqımızda"
-                    tab.setIcon(R.mipmap.icon) // Haqqımızda ikon
+                    tab.setIcon(R.mipmap.icon) // Haqqımızda üçün ikon
                 }
                 1 -> {
                     tab.text = "Dəstək"
-                    tab.setIcon(R.mipmap.icon) // Dəstək ikon
+                    tab.setIcon(R.mipmap.icon) // Dəstək üçün ikon
                 }
             }
         }.attach()
